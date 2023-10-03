480c61c6324926b911e4ce5c5f0b8f10d7ff6a70
tu <chauthitu16tclc3@gmail.com>
2023-10-02 17:14:18 -0700

    main: add project 2023/10/02

---
+++ js/language.js
@@ -0,0 +1,369 @@
+var arrLang = {
+  "en-GB": { 
+    "HEADER_MAIN": "selftaught",
+    "HEADER_SUB": "is how you survive",
+    "NAV_ABOUT": "About us",
+    "NAV_SKILLS": "Our Skills",
+    "NAV_COURSES": "Our courses",
+    "NAV_TESTIMONIALS":"Testimonials",
+    "NAV_ENROLL":"Enroll form",
+    "HEADER_BTN_COURSES":"See our courses",
+    "ABOUT_TITLE":"Story about us",
+    "ABOUT_H3_1":"from zero to hero",
+    "ABOUT_P_1":" Lorem ipsum dolor sit amet,consectetur adipisicing elit.Fuga cumque autem officiis distinctio,voluptate qui minima.Magnam tenetur suscipit eius beatae dolore nihilrepudiandae atque vero,rem, labore ullam ipsa incidunt enim id necessitatibus at dolor! Incidunt sequi nihil amet.",
+    "ABOUT_H3_2":"master your skills",
+    "ABOUT_P_2":"Lorem ipsum dolor sit amet,consectetur adipisicing elite.",
+    "ABOUT_A":"Learn more",
+    "SKILLS_TITLE_1": "global skill",
+    "SKILLS_CONTENT_1": "Lorem ipsum dolor, sit amet consectetur adipisicing elit, Neque, quas doloribus corporis debitis soluta atque.",
+    "SKILLS_TITLE_2": "google's knowledge",
+    "SKILLS_CONTENT_2":"Lorem ipsum dolor, sit amet consectetur adipisicing elit, Neque, quas doloribus corporis debitis soluta atque.",
+    "SKILLS_TITLE_3":"leader of your life",
+    "SKILLS_CONTENT_3":"Lorem ipsum dolor, sit amet consectetur adipisicing elit, Neque, quas doloribus corporis debitis soluta atque.",
+    "SKILLS_TITLE_4":"saving your time",
+    "SKILLS_CONTENT_4":"Lorem ipsum dolor, sit amet consectetur adipisicing elit, Neque, quas doloribus corporis debitis soluta atque.",
+    "COURSES_TITLE":"our courses",
+    "COURSES_TITLE_CONTENT_1":"Control",
+    "COURSES_TITLE_CONTENT_1_1":"your time",
+    "COURSES_HOURS_1":"3 hours",
+    "COURSES_STUDENTS_1":"1000 students",
+    "COURSES_RATED_1":"5 star rated",
+    "COURSES_ONLY_1":"Only",
+    "COURSES_VALUE_1":"$16",
+    "COURSES_A_1":"Enroll now",
+    "COURSES_TITLE_CONTENT_2":"make",
+    "COURSES_TITLE_CONTENT_2_1":"mindmap",
+    "COURSES_HOURS_2":"10 hours",
+    "COURSES_STUDENTS_2":"5000 students",
+    "COURSES_RATED_2":"5 star rated",
+    "COURSES_ONLY_2":"Only",
+    "COURSES_VALUE_2":"$20",
+    "COURSES_A_2":"Enroll now",
+    "COURSES_TITLE_CONTENT_3":"make",
+    "COURSES_TITLE_CONTENT_3_1":"your target",
+    "COURSES_HOURS_3":"15 hours",
+    "COURSES_STUDENTS_3":"5000 students",
+    "COURSES_RATED_3":"5 star rated",
+    "COURSES_VALUE_3":"$25",
+    "COURSES_ONLY_3":"Only",
+    "COURSES_A_3":"Enroll now",
+    "COURSES_BTN_ALL_COURSES":"See all our courses",
+    "TESTIMONIALS_VIDEO":"Your browser is not supported",
+    "TESTIMONIALS_TITLE":"testimonials",
+    "TESTIMONIALS_CAPYION_1":"Lionel Meshi",
+    "TESTIMONIALS_TITLE_1":"thank for your guide",
+    "TESTIMONIALS_CONTENT_1":"Lorem ipsum dolor sit amet consectetur adipisicing elit.Incidunt repellat tenetur officia quis quasi,temporibus neque reiciendis quaerat exercitationem quibusdam?Lorem ipsum dolor sit amet consectetur adipisicing elit.Possimus dolorem labore nam?",
+    "TESTIMONIALS_CAPYION_2":"Hellarri Cshinson",
+    "TESTIMONIALS_TITLE_2":"very helpful course",
+    "TESTIMONIALS_CONTENT_2":"Lorem ipsum dolor sit amet consectetur adipisicing elit.Incidunt repellat tenetur officia quis quasi,temporibus neque reiciendis quaerat exercitationem quibusdam?Lorem ipsum dolor sit amet consectetur adipisicing elit.Possimus dolorem labore nam?",
+    "TESTIMONIALS_A":"read all the testimonials",
+    "ENROLL_TITLE":"enroll now",
+    "ENROLL_NAME":"Full name",
+    "ENROLL_EMAIL":"Email",
+    "ENROLL_MALE":"Male",
+    "ENROLL_FEMALE":"Female",
+    "ENROLL_BTN":"Submit",
+    "FOOTER_LOGO":"SelfTaught",
+    "FOOTER_ADDRESS":"Address",
+    "FOOTER_CONTACTS":"Contacts",
+    "FOOTER_POLICY":"Policy",
+    "FOOTER_SPONSORS":"Sponsors",
+    "FOOTER_SCSS":"SCSS",
+    "POPUP_TITLE":"Enroll now",
+    "POPUP_H3":"Important",
+    "POPUP_H3_1":"Please read these terms before booking.",
+    "POPUP_P":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, ratione dolorum?Dicta quam voluptas ipsum obcaecati laborum mollitia distinctio. Earum quisquam esse est vel aperiam quis quidem commodi voluptatem voluptatum! Magnam quod quis sed fugiat quas. At vel dolorem, officia, rerum tenetur aspernatur, itaque maiores laudantium voluptas assumenda reiciendis nemo!",
+    "POPUP_A":"Enroll now",
+  },
+  "ja-JP": {
+    "HEADER_MAIN": "独学",
+     "HEADER_SUB": "それが生き残る方法です",
+     "NAV_ABOUT": "当社について",
+     "NAV_SKILLS": "当社のスキル",
+     "NAV_COURSES": "弊社のコース",
+     "NAV_TESTIMONIALS":"お客様の声",
+     "NAV_ENROLL":"フォームを登録",
+     "HEADER_BTN_COURSES":"コースを見る",
+     "ABOUT_TITLE":"私たちについてのストーリー",
+     "ABOUT_H3_1":"ゼロからヒーローまで",
+     "ABOUT_P_1":"ヴェルで ドロレム、オフィシア、レルム テネトゥール アスペルナトゥール、イタク マイオーレス ラウダンティウム ヴォルプタス アセンダ ライシエンディス ニモ!",
+     "ABOUT_H3_2":"スキルを習得してください",
+     "ABOUT_P_2":"ロレム・イプサム・ドルー・シット・アメット、コンセクト・アディピシング・エリート。",
+     "ABOUT_A":"詳細はこちら",
+     "SKILLS_TITLE_1": "グローバルスキル",
+     "SKILLS_CONTENT_1": "ロレム・イプサム・ドール、座って、エリートの脂肪を集めて座ってください、首を絞め、体全体のデビティスを解決してください。",
+     "SKILLS_TITLE_2": "Google の知識",
+     "SKILLS_CONTENT_2":"ロレム・イプサム・ドール、座って、エリートの脂肪を集めて座ってください、首を絞め、体全体のデビティスを解決してください。",
+     "SKILLS_TITLE_3":"あなたの人生のリーダー",
+     "SKILLS_CONTENT_3":"ロレム・イプサム・ドール、座って、エリートの脂肪を集めて座ってください、首を絞め、体全体のデビティスを解決してください。",
+     "SKILLS_TITLE_4":"時間を節約します",
+     "SKILLS_CONTENT_4":"ロレム・イプサム・ドール、座ってエリートの脂肪を集めて座って、首を絞め、体全体のデビティスを解決してください。",
+     "COURSES_TITLE":"私たちのコース",
+     "COURSES_TITLE_CONTENT_1":"コントロール",
+     "COURSES_TITLE_CONTENT_1_1":"あなたの時間",
+     "COURSES_HOURS_1":"3 時間",
+     "COURSES_STUDENTS_1":"生徒数 1000 人",
+     "COURSES_RATED_1":"5 つ星の評価",
+     "COURSES_ONLY_1":"のみ",
+     "COURSES_VALUE_1":"$16",
+     "COURSES_A_1":"今すぐ登録してください",
+     "COURSES_TITLE_CONTENT_2":"作る",
+     "COURSES_TITLE_CONTENT_2_1":"マインドマップ",
+     "COURSES_HOURS_2":"10 時間",
+     "COURSES_STUDENTS_2":"生徒数 5000 人",
+     "COURSES_RATED_2":"5 つ星の評価",
+     "COURSES_ONLY_2":"のみ",
+     "COURSES_VALUE_2":"$20",
+     "COURSES_A_2":"今すぐ登録",
+     "COURSES_TITLE_CONTENT_3":"作る",
+     "COURSES_TITLE_CONTENT_3_1":"あなたの目標",
+     "COURSES_HOURS_3":"15 時間",
+     "COURSES_STUDENTS_3":"生徒数 5000 人",
+     "COURSES_RATED_3":"5 つ星の評価",
+     "COURSES_VALUE_3":"$25",
+     "COURSES_ONLY_3":"のみ",
+     "COURSES_A_3":"今すぐ登録してください",
+     "COURSES_BTN_ALL_COURSES":"すべてのコースを表示",
+     "TESTIMONIALS_VIDEO":"お使いのブラウザはサポートされていません",
+     "TESTIMONIALS_TITLE":"お客様の声",
+     "TESTIMONIALS_CAPYION_1":"ライオネル飯",
+     "TESTIMONIALS_TITLE_1":"ガイドをありがとう",
+     "TESTIMONIALS_CONTENT_1":"ロレム・イプサム・ドール、座って、エリートの脂肪を集めて座ってください、首を絞め、体全体のデビティスを解決してください。",
+     "TESTIMONIALS_CAPYION_2":"ヘラリ・クシンソン",
+     "TESTIMONIALS_TITLE_2":"とても役立つコース",
+     "TESTIMONIALS_CONTENT_2":"ロレム・イプサム・ドール、座って、エリートの脂肪を集めて座ってください、首を絞め、体全体のデビティスを解決してください。",
+     "TESTIMONIALS_A":"すべてのお客様の声を読む",
+     "ENROLL_TITLE":"今すぐ登録",
+     "ENROLL_NAME":"フルネーム",
+     "ENROLL_EMAIL":"メールアドレス",
+     "ENROLL_MALE":"男性",
+     "ENROLL_FEMALE":"女性",
+     "ENROLL_BTN":"送信",
+     "FOOTER_LOGO":"独学",
+     "FOOTER_ADDRESS":"住所",
+     "FOOTER_CONTACTS":"連絡先",
+     "FOOTER_POLICY":"ポリシー",
+     "FOOTER_SPONSORS":"スポンサー",
+     "FOOTER_SCSS":"SCSS",
+     "POPUP_TITLE":"今すぐ登録",
+     "POPUP_H3":"重要",
+     "POPUP_H3_1":"予約する前にこれらの規約をお読みください。",
+     "POPUP_P":"ロレム・イプサム・ドール、座って、エリートの脂肪を集めて座ってください、首を絞め、体全体のデビティスを解決してください。",
+     "POPUP_A":"今すぐ登録してください",
+  }
+};
+
+
+const locales = ["en-GB","ja-JP"];
+var language = "en-GB";
+
+$(document).ready(function() {
+  language = "en-GB";
+  $(".language").each(function(index, element) {
+    $(this).text(arrLang[language][$(this).attr("key")]);
+  });
+});
+
+function getFlagSrc(countryCode) {
+  return /^[A-Z]{2}$/.test(countryCode)
+    ? `https://www.countryflagicons.com/SHINY/64/${countryCode.toUpperCase()}.png`
+    : "";
+}
+
+const dropdownBtn = document.getElementById("dropdown-btn");
+const dropdownContent = document.getElementById("dropdown-content");
+const dropdownContentMb = document.getElementById("dropdown-content-mb");
+
+function setSelectedLocalePC(locale) {
+  const intlLocale = new Intl.Locale(locale);
+  language= intlLocale.baseName;
+  const langName = new Intl.DisplayNames([locale], {
+    type: "language",
+  }).of(intlLocale.language);
+  
+
+  dropdownContent.innerHTML = "";
+
+  const otherLocales = locales.filter((loc) => loc !== locale);
+  otherLocales.forEach((otherLocale) => {
+    const otherIntlLocale = new Intl.Locale(otherLocale);
+    const otherLangName = new Intl.DisplayNames([otherLocale], {
+      type: "language",
+    }).of(otherIntlLocale.language);
+    const listEl = document.createElement("li");
+    listEl.className =`translate`;
+    listEl.id=`${otherLocale}`;
+    listEl.innerHTML = `${otherLangName}<img src="${getFlagSrc(
+      otherIntlLocale.region
+    )}" />`;
+    listEl.value = otherLocale;
+    listEl.addEventListener("mousedown", function () {
+      setSelectedLocalePC(otherLocale);
+    });
+    dropdownContent.appendChild(listEl);
+  });
+
+  dropdownBtn.innerHTML = `<img src="${getFlagSrc(
+    intlLocale.region
+  )}" /><span class="arrow-down"></span>`;
+
+  const idText=new Intl.Locale(locale);
+  $(".language").each(function(index, element) {
+    $(this).text(arrLang[idText][$(this).attr("key")]);
+  });
+
+}
+
+function setSelectedLocaleMb(locale) {
+  const intlLocale = new Intl.Locale(locale);
+  language= intlLocale.baseName;
+  const langName = new Intl.DisplayNames([locale], {
+    type: "language",
+  }).of(intlLocale.language);
+  
+  dropdownContentMb.innerHTML = "";
+
+  const otherLocales = locales.filter((loc) => loc !== locale);
+  otherLocales.forEach((otherLocale) => {
+    const otherIntlLocale = new Intl.Locale(otherLocale);
+    const otherLangName = new Intl.DisplayNames([otherLocale], {
+      type: "language",
+    }).of(otherIntlLocale.language);
+    const listEl = document.createElement("span");
+    listEl.className =`translate `;
+    listEl.id=`${otherLocale}`;
+    listEl.innerHTML = `<img class="ius-icon-language-mb" src="${getFlagSrc(
+      otherIntlLocale.region
+    )}" />${otherLangName}`;
+    listEl.value = otherLocale;
+    listEl.addEventListener("mousedown", function () {
+      setSelectedLocaleMb(otherLocale);
+    });
+    dropdownContentMb.appendChild(listEl);
+  });
+
+  // dropdownBtnMb.innerHTML = `<img src="${getFlagSrc(
+  //   intlLocale.region
+  // )}" /><span class="arrow-down"></span>`;
+
+  const idText=new Intl.Locale(locale);
+  $(".language").each(function(index, element) {
+    $(this).text(arrLang[idText][$(this).attr("key")]);
+  });
+
+}
+
+function setSelectedLocale(){
+  if(window.innerWidth < 1200){
+    setSelectedLocaleMb(locales[0]);
+    const browserLang = new Intl.Locale(navigator.language).language;
+    for (const locale of locales) {
+      const localeLang = new Intl.Locale(locale).language;
+      if (localeLang === browserLang) {
+        setSelectedLocaleMb(locale);
+      }
+    }
+  }else{
+    setSelectedLocalePC(locales[0]);
+    const browserLang = new Intl.Locale(navigator.language).language;
+    for (const locale of locales) {
+      const localeLang = new Intl.Locale(locale).language;
+      if (localeLang === browserLang) {
+        setSelectedLocalePC(locale);
+      }
+    }
+    
+  }
+}
+
+
+window.addEventListener('load',(event) => {
+  setSelectedLocale();
+});
+
+window.addEventListener('resize', function() {
+  setSelectedLocale();
+});
+
+
+
+
+// const locales = ["en-GB","ja-JP"];
+// var language = "en-GB";
+
+// $(document).ready(function() {
+//   language = "en-GB";
+//   $(".language").each(function(index, element) {
+//     $(this).text(arrLang[language][$(this).attr("key")]);
+//   });
+// });
+
+// function getFlagSrc(countryCode) {
+//   return /^[A-Z]{2}$/.test(countryCode)
+//     ? `https://www.countryflagicons.com/SHINY/64/${countryCode.toUpperCase()}.png`
+//     : "";
+// }
+
+// const dropdownBtn = document.getElementById("dropdown-btn");
+// const dropdownContent = document.getElementById("dropdown-content");
+
+// function setSelectedLocale(locale) {
+//   const intlLocale = new Intl.Locale(locale);
+//   language= intlLocale.baseName;
+//   const langName = new Intl.DisplayNames([locale], {
+//     type: "language",
+//   }).of(intlLocale.language);
+  
+
+//   dropdownContent.innerHTML = "";
+
+//   const otherLocales = locales.filter((loc) => loc !== locale);
+//   otherLocales.forEach((otherLocale) => {
+//     const otherIntlLocale = new Intl.Locale(otherLocale);
+//     const otherLangName = new Intl.DisplayNames([otherLocale], {
+//       type: "language",
+//     }).of(otherIntlLocale.language);
+//     const listEl = document.createElement("li");
+//     listEl.className =`translate`;
+//     listEl.id=`${otherLocale}`;
+//     listEl.innerHTML = `<img src="${getFlagSrc(
+//       otherIntlLocale.region
+//     )}"/> ${otherLangName}`;
+//     listEl.value = otherLocale;
+//     listEl.addEventListener("mousedown", function () {
+//       setSelectedLocale(otherLocale);
+//     });
+//     dropdownContent.appendChild(listEl);
+//   });
+
+//   dropdownBtn.innerHTML = `<img src="${getFlagSrc(
+//     intlLocale.region
+//   )}" /><span class="arrow-down"></span>`;
+
+//   const idText=new Intl.Locale(locale);
+//   $(".language").each(function(index, element) {
+//     $(this).text(arrLang[idText][$(this).attr("key")]);
+//   });
+
+// }
+
+// function setLanguage(){
+//   setSelectedLocale("en-GB");
+//     const browserLang = new Intl.Locale(navigator.language).language;
+//     for (const locale of locales) {
+//       const localeLang = new Intl.Locale(locale).language;
+//       if (localeLang === browserLang) {
+//         setSelectedLocale(locale);
+//       }
+//     }
+
+// }
+
+
+// window.addEventListener('load',(event) => {
+//   setLanguage();
+// });
+
+// window.addEventListener('resize', function() {
+//   setLanguage();
+// });
---
+++ readme.md
@@ -0,0 +1,6 @@
+Step1:
+    npm init
+Step2:
+    npm install --save-dev node-sass
+Step3:
+    npm run sasstocss
